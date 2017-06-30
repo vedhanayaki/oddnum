437f95f45e4c876b10566f472cd88dc263f9ab9c
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-30 11:36:49 +0530

    Add files via upload

---
+++ oddnumbtw2intervals.java
@@ -0,0 +1,15 @@
+package guviset3;
+
+public class oddnumbtw2intervals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=100;
+for(int i=0;i<100;i++){
+	if(i%2==1){
+		System.out.println(i);
+	}
+}
+	}
+
+}
